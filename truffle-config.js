a2ba568f21878fdcd93b866e1154a4a28bcdeeda
suum-lion <suum@likelion.net>
2021-05-27 16:03:14 +0900

    FIX: local develop

--- truffle-config.js
+++ truffle-config.js
@@ -1,18 +1,14 @@
-require("dotenv").config();
-const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
 const path = require("path");
-
-const NETWORK_ID = "1001";
-const URL = "https://api.baobab.klaytn.net:8651";
-const GASLIMIT = "10000000";
-
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const NETWORK_ID = "5777";
+const GASLIMIT = "1000000";
 
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    baobab: {
-      provider: () => new HDWalletProvider(PRIVATE_KEY, URL),
+    develop: {
+      host: "127.0.0.1",
+      port: 7545,
+      from: "0xEd1067cf8cc8a721a3CcD18423756978C43D3d65",
       network_id: NETWORK_ID,
       gas: GASLIMIT,
       gasPrice: null
